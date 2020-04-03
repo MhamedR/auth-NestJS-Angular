6a8d7da154d7214b9496ed5b8bee79f6a828f0ab
MhamedR <mhamed.radhouane@gmail.com>
2020-04-03 21:50:01 +0100

    changes

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IUser } from '../../shared/_models/user.model';
 import { AuthenticationService } from '../../shared/_services/authentication.service'
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,7 +10,7 @@ import { AuthenticationService } from '../../shared/_services/authentication.ser
 export class LoginComponent implements OnInit {
 
   user = new Object() as IUser;
-  constructor(private authenticationService: AuthenticationService) {
+  constructor(private authenticationService: AuthenticationService, private router : Router) {
     this.user;
   }
   /**
@@ -17,7 +18,9 @@ export class LoginComponent implements OnInit {
    */
   onSubmit(user:IUser) {
     this.authenticationService.login(user.username, user.password).subscribe(res => {
-      console.log("result authentification : ", res)
+      if(res){
+        this.router.navigate(['/home'])
+      }
     })
 
   }
